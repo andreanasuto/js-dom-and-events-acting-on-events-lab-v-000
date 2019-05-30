c80b8fef2eb8cd73fee3c83af0e756cbb4f6c2ef
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-30 19:05:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function addNewElementAsLi() {
 
 function addNewLiOnClick() {
   return $('input[type="submit"]').click(function () {
-    addNewElementAsLi()
+    return addNewElementAsLi()
   })
 }
 
